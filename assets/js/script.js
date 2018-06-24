69279ca475c5ea4b51a588913e4181ceb5795c12
dominiquem <dominique.meaux@gmail.com>
2018-06-24 22:13:40 +0200

    afficher et masquer un block

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//fonction permettant d afficher et de masquer un block
+function display () {
+  document.getElementById('text').style.display = 'block';
+}
+function mascarade () {
+  document.getElementById('text').style.display = 'none';
+
+}
